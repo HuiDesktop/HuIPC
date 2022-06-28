1395a9af6bf3cb368740ce8561deeda91291a9af
Pure01Fx <pure01fx@outlook.com>
2022-06-28 11:08:09 +0800

    fix(randstr): randstr may get chr=0

--- src/tools.c
+++ src/tools.c
@@ -9,7 +9,7 @@ void randStr(char* c, size_t from, size_t to) {
 		is_srand = 1;
 	}
 	while (from != to) {
-		c[from] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"[rand() % 63];
+		c[from] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"[rand() % 62];
 		from += 1;
 	}
 	c[to] = '\0';
